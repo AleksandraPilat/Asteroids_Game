eb56960e1f506f5ff6ee46502a131e855ff9bdf5
Aleksandra Piłat <aleksandrapilat.contact@gmail.com>
2025-06-07 19:03:14 +0200

    finished game

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collide(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
 
         for obj in drawable:
